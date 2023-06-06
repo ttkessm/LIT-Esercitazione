adbbf3899b5bab9ff405f2c1b176cdd4527a12c4
ttkessm <126757281+ttkessm@users.noreply.github.com>
2023-06-06 13:22:06 +0200

    Added main.cpp for testing.

---
+++ main.cpp
@@ -0,0 +1,38 @@
+
+/// \file main.cpp
+///     \brief Main function of the program
+///     \return 0 if the program executed successfully
+///
+///     Details.
+///
+
+#include <iostream>
+#include <fstream>
+#include "cesare.h"
+
+using namespace std;
+
+int main()
+{
+    char inputText[1000]; /// Input text entered by the user
+    int shiftVal;         /// \shiftVal Shift value for the Caesar cipher
+
+    cout << "Enter the text to encrypt: ";
+    cin.getline(inputText, 1000); /// Read the input text from the user
+
+    cout << "Enter the shift value: ";
+    cin >> shiftVal; /// Read the shift value
+
+    Cesare cipher(inputText, shiftVal); /// Create a Cesare object using the input text and shift value
+
+    char *encryptedText = cipher.Cipher(inputText);       /// Encrypt the input text using the Cipher() method
+    char *decryptedText = cipher.Decipher(encryptedText); /// Decrypt the encrypted text using the Decipher() method
+
+    cout << "Encrypted text: " << encryptedText << endl; /// Print the encrypted text
+    cout << "Decrypted text: " << decryptedText << endl; /// Print the decrypted text
+
+    delete[] encryptedText; /// Deallocate the memory for the encrypted text
+    delete[] decryptedText; /// Deallocate the memory for the decrypted text
+
+    return 0; /// Return 0 to indicate successful program execution
+}
