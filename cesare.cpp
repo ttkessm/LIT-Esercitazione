84817a686a5a38f3c118f481b06d8c7c40f2dc49
disodiso <127971757+disodiso@users.noreply.github.com>
2023-06-06 16:56:36 +0200

    Update cesare.cpp

--- cesare.cpp
+++ cesare.cpp
@@ -4,154 +4,295 @@
 ///               Derivate of class ciphertext
 ///
 
+
 #include <iostream>
+
 #include <cstring>
-#include <string.h>
+
 #include "cesare.h"
+
+
+
 
 using namespace std;
 
-///@brief Default constructor
+
+
+
+// Default constructor
+
 Cesare::Cesare() : Ciphertext()
+
 {
-    cout << "Cesare - Default constructor" << endl;
-    shift = 0;
+
+	cout << "Cesare - Default constructor" << endl;
+
+	shift = 0;
+
 }
 
-/// @brief Constructor with shift value
-/// @param text The input text
-/// @param shiftVal The shift value for the Caesar cipher
-Cesare::Cesare(char *text, int shiftVal) : Ciphertext(text)
+
+
+
+// Constructor with shift value
+
+Cesare::Cesare(char* text, int shiftVal) : Ciphertext(text)
+
 {
-    cout << "Cesare - Constructor" << endl;
-    shift = shiftVal;
+
+	cout << "Cesare - Constructor" << endl;
+
+	shift = shiftVal;
+
 }
 
-/// @brief Copy constructor
-/// @param o Reference to the object to be copied
-Cesare::Cesare(const Cesare &o) : Ciphertext(o)
+
+
+
+// Copy constructor
+
+Cesare::Cesare(const Cesare& o) : Ciphertext(o)
+
 {
-    cout << "Cesare - Copy constructor" << endl;
-    shift = o.shift;
+
+	cout << "Cesare - Copy constructor" << endl;
+
+	shift = o.shift;
+
 }
 
-///@brief Destructor
+
+
+
+// Destructor
+
 Cesare::~Cesare()
+
 {
-    cout << "Cesare - Destructor" << endl;
+
+	cout << "Cesare - Destructor" << endl;
+
 }
 
-/// @brief Overloaded assignment operator
-/// @param o Reference to the object on the right of the operator
-/// @return Reference to the object on the left of the operator
-Cesare &Cesare::operator=(const Cesare &o)
+
+
+
+// Overloaded assignment operator
+
+Cesare& Cesare::operator=(const Cesare& o)
+
 {
-    cout << "Cesare - Assignment operator" << endl;
-    if (this != &o)
-    {
-        Ciphertext::operator=(o);
-        shift = o.shift;
-    }
-    return *this;
+
+	cout << "Cesare - Assignment operator" << endl;
+
+	if (this != &o)
+
+	{
+
+		Ciphertext::operator=(o);
+
+		shift = o.shift;
+
+	}
+
+	return *this;
+
 }
 
-/// @brief Set the shift value
-/// @param shiftVal The new shift value for the Caesar cipher
+
+
+
+// Set the shift value
+
 void Cesare::SetShift(int shiftVal)
+
 {
-    shift = shiftVal;
+
+	shift = shiftVal;
+
 }
 
-/// @brief Get the shift value
-int Cesare::GetShift()
-{
-    int shiftInput;
-    cout << "Enter shift value: ";
-    cin >> shiftInput;
 
-    return shiftInput;
+
+
+// Override the base class's Cipher function
+
+char* Cesare::Cipher(char* text)
+
+{
+
+	int length = strlen(text);
+
+	char* cipherText = new char[length + 1];
+
+
+
+
+	do
+
+	{
+
+		cout << "Enter shift value for ciphering (positive number): ";
+
+		cin >> shift;
+
+
+
+
+		if (shift <= 0)
+
+		{
+
+			cout << "Invalid shift value. Please enter a positive number." << endl;
+
+		}
+
+	} while (shift <= 0);
+
+
+
+
+	// Clean the input string
+
+	int j = 0;
+
+	for (int i = 0; i < length; i++)
+
+	{
+
+		if (isalpha(text[i]))
+
+		{
+
+			if (isupper(text[i]))
+
+				cipherText[j] = tolower(text[i]);
+
+			else
+
+				cipherText[j] = text[i];
+
+			j++;
+
+		}
+
+	}
+
+	cipherText[j] = '\0';
+
+
+
+
+	// Apply the Caesar cipher
+
+	for (int i = 0; i < j; i++)
+
+	{
+
+		if (islower(cipherText[i]))
+
+			cipherText[i] = (cipherText[i] - 'a' + shift) % 26 + 'a';
+
+	}
+
+
+
+
+	return cipherText;
+
 }
 
-/// @brief Removes special characters and makes uppercase characters lowercase
-/// @param text The input text
-/// @return The text ready to be ciphered
-char *Cesare::CleanText(char *text)
+
+
+
+// Override the base class's Decipher function
+
+char* Cesare::Decipher(char* text)
+
 {
-    int i = 0; ///@param i counter variable
-    int k = 0; ///@param k indesired characters variable
-    unsigned int ascii;
-    char *cleanedText;
 
-    cleanedText = new char[strlen(text) + 1];      // create an array with the same lenght as the text array
-    strcpy_s(cleanedText, strlen(text) + 1, text); // copy the text on that array
+	int length = strlen(text);
 
-    while (cleanedText[i] != '\0' && (i < strlen(cleanedText) + 1 - k)) // funciton that detects ASCII simbols which are not alfabetic charcaters,
-    {                                                                   // and makes uppercase characters shift by 32 on the ASCII table, making them lowercase
-        ascii = (unsigned int)cleanedText[i];
+	char* decipherText = new char[length + 1];
 
-        if (ascii >= 97 && ascii <= 122)
-            cleanedText[i] = (char)ascii;
 
-        else if (ascii >= 65 && ascii <= 90)
-            cleanedText[i] = (char)(ascii + 32);
 
-        else
-            k++;
 
-        i++;
-    }
+	do
 
-    return cleanedText;
+	{
+
+		cout << "Enter shift value for deciphering (positive number): ";
+
+		cin >> shift;
+
+
+
+
+		if (shift <= 0)
+
+		{
+
+			cout << "Invalid shift value. Please enter a positive number." << endl;
+
+		}
+
+	} while (shift <= 0);
+
+
+
+
+	// Clean the input string
+
+	int j = 0;
+
+	for (int i = 0; i < length; i++)
+
+	{
+
+		if (isalpha(text[i]))
+
+		{
+
+			if (isupper(text[i]))
+
+				decipherText[j] = tolower(text[i]);
+
+			else
+
+				decipherText[j] = text[i];
+
+			j++;
+
+		}
+
+	}
+
+	decipherText[j] = '\0';
+
+
+
+
+	// Apply the Caesar cipher (reverse shift)
+
+	for (int i = 0; i < j; i++)
+
+	{
+
+		if (islower(decipherText[i]))
+
+			decipherText[i] = (decipherText[i] - 'a' - shift + 26) % 26 + 'a';
+
+	}
+
+
+
+
+	return decipherText;
+
 }
 
-/// @brief Override the base class's Cipher function
-/// @param text The input clean text
-/// @return The ciphered text
-char *Cesare::Cipher(char *text)
-{
-    char *cleanedText = CleanText(text);
-    int i = 0;
-    unsigned int shift;
-    unsigned int ascii;
 
-    char *cipherText = new char[strlen(cleanedText) + 1];
-    strcpy_s(cipherText, strlen(cleanedText) + 1, cleanedText);
 
-    shift = GetShift();
 
-    while (cipherText[i] != '\0' && (i < strlen(cipherText) + 1))
-    {
-        ascii = (unsigned int)cipherText[i];
-        cipherText[i] = (char)(97 + (ascii + shift - 97) % 26);
 
-        i++;
-    }
-
-    return cipherText;
-}
-
-/// @brief Override the base class's Decipher function
-/// @param text The ciphered text
-/// @return The deciphered text
-char *Cesare::Decipher(char *text)
-{
-    int i = 0;
-    unsigned int shift;
-    unsigned int ascii;
-    char *decipherText;
-
-    int length = strlen(text);
-    decipherText = new char[length + 1];
-
-    shift = GetShift();
-
-    while (text[i] != '\0' && (i < strlen(text) + 1))
-    {
-        ascii = (unsigned int)decipherText[i];
-        decipherText[i] = (char)(97 + (ascii - 97 - shift + 26) % 26);
-
-        i++;
-    }
-
-    return decipherText;
-}
